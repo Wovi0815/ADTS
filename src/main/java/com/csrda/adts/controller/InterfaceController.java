a40663b767506c7015f51da508c047427dfa0979
wangwei <18210193887@163.com>
2020-04-01 14:28:53 +0800

    #接口菜单中间件动态生成，前端预留报文动态生成

--- src/main/java/com/csrda/adts/controller/InterfaceController.java
+++ src/main/java/com/csrda/adts/controller/InterfaceController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.csrda.adts.service.InterfaceService;
 
 @Controller
@@ -16,6 +18,7 @@ private InterfaceService interfaceService;
 	
 	//查询中间件
 	@RequestMapping("/QueryMidware.do")
+	@ResponseBody
 	public List<Map<String,Object>> QueryMidware() {
 		List<Map<String, Object>> result = interfaceService.qryMidware();
 		System.out.println("result"+result);
