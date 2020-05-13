2b07e502e429e19a834d67c0787374b29ffe35ab
朱光超 <1278609056@qq.com>
2020-05-13 22:33:52 +0800

    fix # addstruct

--- src/main/java/com/csrda/adts/controller/dataTypeController.java
+++ src/main/java/com/csrda/adts/controller/dataTypeController.java
@@ -1,5 +1,6 @@
 package com.csrda.adts.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.csrda.adts.dao.TypeDataDao;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class dataTypeController {
@@ -41,6 +46,21 @@ public class dataTypeController {
 	
 	@RequestMapping("/addStruct")
 	public String addStruct() {
+		
+		return "addStruct";
+	}
+	
+	@RequestMapping("/addStructData")
+	@ResponseBody
+	public String addStructData(String typId,String typName,String typSize,String typDesc,String memList) throws Exception, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		List<Map<String, Object>> memData = mapper.readValue(memList, new TypeReference<List<Map<String, Object>>>(){});
+		System.out.println(memList.toString());
+		System.out.println(typId);
+		System.out.println(typName);
+		System.out.println(typSize);
+		System.out.println(typDesc);
+		
 		return "addStruct";
 	}
 	
